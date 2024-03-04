5b916e7176be7a0381d5480e3a386259a0c7af03
alldaygzchen <gonzalochen66@gmail.com>
2024-03-04 08:10:27 +0800

    0304 128 done

--- practice/hash/128_m_longest_consecutive_sequence.py
+++ practice/hash/128_m_longest_consecutive_sequence.py
@@ -14,25 +14,26 @@ class Solution:
 
         while True:
             # print('#######')
-            # print('prev:',prev,'max_length',max_length)
+            # print('prev:',prev,'prev_max_length',max_length)
             # print('nums[l]:',nums[l],'nums[r]:',nums[r])
 
             if r == len(nums):
                 return max_length
             
-            if prev and prev+1!=nums[r]:
-                max_length = max(r-l+1,max_length)
+            if prev is not None  and prev+1!=nums[r]:
+                # max_length = max(r-1-l+1,max_length)
                 prev = nums[r]
-                r+=1
                 l=r
+                r+=1
                 continue
-
+            max_length = max(r-l+1,max_length)
             prev = nums[r]
             r+=1
             
     
 if __name__=="__main__":
     nums =[[4,0,-4,-2,2,5,2,0,-8,-8,-8,-8,-1,7,4,5,5,-4,6,6,-3],[9,1,4,7,3,-1,0,5,8,-1,6],[100,4,200,1,3,2],[0,3,7,2,5,8,4,6,0,1]]
+    # nums =[[9,1,4,7,3,-1,0,5,8,-1,6]]
     for num in nums:
         sol =Solution()
         print('output',sol.longestConsecutive(num))
