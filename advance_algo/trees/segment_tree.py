a5542e93e9ec70981fa831ea91076738bd508809
alldaygzchen <gonzalochen66@gmail.com>
2024-03-05 23:48:19 +0800

    0305 segment tree review

---
+++ advance_algo/trees/segment_tree.py
@@ -0,0 +1,70 @@
+class SegmentTree:
+    def __init__(self,sum,L,R):
+        
+        # node
+        self.left = None
+        self.right = None
+
+        # range
+        self.L = L
+        self.R = R
+
+        # sum
+        self.sum = sum
+
+    @staticmethod
+    def build(nums,L,R):
+
+        """
+        Every recursion purpose is to pass the value e.g. L, R
+        
+        """
+
+        # base case
+        if L==R:
+            return SegmentTree(nums[L],L,R)
+        
+        M = (L+R)//2
+        root = SegmentTree(0,L,R)
+        root.left = SegmentTree.build(nums,L,M)
+        root.right = SegmentTree.build(nums,M+1,R)
+        root.sum = root.left.sum + root.right.sum
+        return root
+    
+    def update(self,index,val):
+        
+        # base case
+        if index == self.L == self.R:
+            self.sum =  val
+            return 
+
+        M = (self.L + self.R) //2
+        if index>M:
+            self.right.update(index,val)    
+        else:
+            self.left.update(index, val)
+        self.sum = self.left.sum + self.right.sum
+
+
+    def rangeQuery(self,L,R):
+        
+        # base case
+        if L == self.L and R==self.R:
+            return self.sum
+        
+        M = (self.L + self.R) //2
+        if L>M:
+            return self.right.rangeQuery(L, R)
+        elif R <= M:
+            return self.left.rangeQuery(L, R)
+        
+        else:
+            return (self.left.rangeQuery(L, M) +
+                    self.right.rangeQuery(M + 1, R))
+
+
+        
+
+        
+
+
